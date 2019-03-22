ba4183d76253712811ebb7dfb5c0b4c00a0376b7
class03 <class03@T03-01>
2019-03-22 17:01:33 +0900

    03/22 오후 커밋...

---
+++ class03_T/src/day04/Test08.java
@@ -0,0 +1,16 @@
+package day04;
+
+public class Test08 {
+/*
+ * 문제]
+ * 		1년은 365.2426 일 입니다.
+ * 		이것을 몇일 , 몇시간, 몇분, 몇초인지 계산하세요.
+ * 
+ * 		참고]
+ * 				0.5일은 12시간입니다.
+ */
+	public static void main(String[] args) {
+		
+	}
+
+}
---
+++ class03_T/src/day05/Test01.java
@@ -0,0 +1,9 @@
+package day05;
+
+public class Test01 {
+
+	public static void main(String[] args) {
+		
+	}
+
+}
