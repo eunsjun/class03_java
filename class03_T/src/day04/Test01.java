e8c776c905c6ec9169fdcf30730c7f636b13d567
class03 <class03@T03-01>
2019-03-22 13:33:21 +0900

    03/22 오전 커밋...

---
+++ class03_T/src/day04/Test01.java
@@ -0,0 +1,11 @@
+package day04;
+
+public class Test01 {
+
+	public static void main(String[] args) {
+		byte num = 127 ;
+		
+		System.out.println("num : " + (byte)(num + 1));
+	}
+
+}
---
+++ class03_T/src/day04/Test05.java
@@ -0,0 +1,28 @@
+package day04;
+
+public class Test05 {
+	/*
+	 * 문제] 하나의 문자를 입력 받아서
+	 * 		 그 문자가 대문자인지, 소문자 인지, 아니면 다른문자인지
+	 * 		 출력해 보세요.
+	 * 		삼항연산자를 사용해서 처리하세요.
+	 */
+	public static void main(String[] args) {
+		
+	}
+	
+	/*
+		String str = "Hello World";
+		
+		  charAt(인덱스) ==> 문자열의 인덱스에 해당하는 문자를 반환
+		  
+		  		문자열.charAt(숫자)
+		  
+		  참고]
+		  		모든 프로그래밍 언어는 순서가 0부터 시작한다.
+		 
+		System.out.println("다섯번째 문자 : " + str.charAt(4));
+	 * 
+	 */
+
+}
