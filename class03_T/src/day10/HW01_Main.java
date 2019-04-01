1f7d39b45da1d04bdb79e7199e793427bbe534f6
class03 <class03@T03-01>
2019-04-01 18:27:24 +0900

    5th commit

--- class03_T/src/day10/HW01_Main.java
+++ class03_T/src/day10/HW01_Main.java
@@ -2,16 +2,24 @@ package day10;
 
 public class HW01_Main {
 	HW01 score ;
+	
 	public HW01_Main() {
 		score = new HW01();
 		
 		for(int i = 0 ; i < score.class03.length ; i++ ) {
 			System.out.println("학생 번호 : " + score.class03[i][0]);
 		}
+		
+		int rad = (int)(Math.random()*101);
+	}
+	
+	public HW01_Main(String str) {
+		System.out.println(str);
 	}
 
 	public static void main(String[] args) {
 		new HW01_Main();
+//		new HW01_Main("제자님 공부하세요!");
 	}
 
 }
--- class03_T/src/day10/HW02.java
+++ class03_T/src/day10/HW02.java
@@ -13,10 +13,23 @@ public class HW02 {
  * 
  * 		HW02[] ban = new HW02[15];
  */
+	int no, java, oracle, jsp, spring, total;
+	
 	public HW02() {
+		this(getNo(), getScore(), getScore(), getScore(), getScore());
+	}
+	
+	public HW02(int n, int sc1, int sc2, int sc3, int sc4) {
 		
 	}
-
+	
+	public static int getNo() {
+		return (int)(Math.random()*15 + 1);
+	}
+	
+	public static int getScore() {
+		return (int)(Math.random()*101);
+	}
 	public static void main(String[] args) {
 		new HW02();
 	}
